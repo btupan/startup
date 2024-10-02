638cfb2bc89c28386bebf039272ed7d196017bda
btupan <115275767+btupan@users.noreply.github.com>
2024-10-02 16:44:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,211 @@
+function openModal(ReasonNumber) {
+    const modalBody = document.querySelector('.modal-content');
+    switch(ReasonNumber){
+        case 1:
+            modalBody.innerHTML = '<h2>Je bent perfect</h2>';
+            modalBody.innerHTML += '<p>De meeste perfecte meid die er bestaat!</p>';
+        break;
+        case 2:
+            modalBody.innerHTML = '<h2>Je bent mooi</h2>';
+            modalBody.innerHTML += '<p>De mooiste meid die er bestaat!</p>';
+        break;
+        case 3:
+            modalBody.innerHTML = '<h2>Je hebt een leuke persoonlijkheid</h2>';
+            modalBody.innerHTML += '<p>Er is niemand zoals jou!</p>';
+        break;
+        case 4:
+            modalBody.innerHTML = '<h2>Je hebt een mooi gezicht!</h2>';
+            modalBody.innerHTML += '<p>Gezicht van een engel!</p>';
+        break;
+        case 5:
+            modalBody.innerHTML = '<h2>Je bent behulpzaam</h2>';
+            modalBody.innerHTML += '<p>Wanneer je om iemand geeft zet je diegene boven jezelf!</p>';
+        break;
+        case 6:
+            modalBody.innerHTML = '<h2>Je hebt een mooie neus</h2>';
+            modalBody.innerHTML += '<p>Uhm ja. Dat vind ik gewoon.</p>';
+        break;
+        case 7:
+            modalBody.innerHTML = '<h2>Je hebt mooi haar</h2>';
+            modalBody.innerHTML += '<p>Mooi stralend haar!</p>';
+        break;
+        case 8:
+            modalBody.innerHTML = '<h2>Je haar ruikt lekker</h2>';
+            modalBody.innerHTML += '<p>Altijd een heerlijke geur!</p>';
+        break;
+        case 9:
+            modalBody.innerHTML = '<h2>Je hebt een mooi lichaam</h2>';
+            modalBody.innerHTML += '<p>Elk lichaamsdeel echt hoe dan??</p>';
+        break;
+        case 10:
+            modalBody.innerHTML = '<h2>Je bent meer dan goed genoeg voor mij</h2>';
+            modalBody.innerHTML += '<p>Vergeet dat nooit Shaun.</p>';
+        break;
+        case 11:
+            modalBody.innerHTML = '<h2>Je bent creatief</h2>';
+            modalBody.innerHTML += '<p>Al die cadeaus en dingen die je op school hebt gemaakt. Niet te geloven.</p>';
+        break;
+        case 12:
+            modalBody.innerHTML = '<h2>Je hebt een zeer schone huid</h2>';
+            modalBody.innerHTML += '<p>Echt super glad!</p>';
+        break;
+        case 13:
+            modalBody.innerHTML = '<h2>Je bent geil (lekker)</h2>';
+            modalBody.innerHTML += '<p>Uhm ja...</p>';
+        break;
+        case 14:
+            modalBody.innerHTML = '<h2>Je hebt een dikke kont</h2>';
+            modalBody.innerHTML += '<p>Uhm ja...</p>';
+        break;
+        case 15:
+            modalBody.innerHTML = '<h2>Je hebt gladde benen</h2>';
+            modalBody.innerHTML += '<p>Ook als je niet scheert!</p>';
+        break;
+        case 16:
+            modalBody.innerHTML = '<h2>Je bent mooi</h2>';
+            modalBody.innerHTML += '<p>De mooiste meid die er bestaat!</p>';
+        break;
+        case 17:
+            modalBody.innerHTML = '<h2>Je bent een parel</h2>';
+            modalBody.innerHTML += '<img src="O:\\Shaun 20th Birthday Project\\Images\\Parel.jpg" width="100" height="100" style="border-radius:50%;">';
+        break;
+        case 18:
+            modalBody.innerHTML = '<h2>Je hebt leuke knuffels</h2>';
+            modalBody.innerHTML += '<img src="O:\\Shaun 20th Birthday Project\\Images\\Aapje.png" width="100" height="100" style="border-radius:50%;">';
+        break;
+        case 19:
+            modalBody.innerHTML = '<h2>Je hebt een leuke kledingstijl</h2>';
+            modalBody.innerHTML += '<p>Elke dag zie jij er leuk uit!</p>';
+        break;
+        case 20:
+            modalBody.innerHTML = '<h2>Je hebt geen makeup nodig</h2>';
+            modalBody.innerHTML += '<p>Net zoals 99,9% van alle meiden tegenwoordig</p>';
+        break;
+        case 21:
+            modalBody.innerHTML = '<h2>Je laat zien dat je om me geeft</h2>';
+            modalBody.innerHTML += '<p>En ik weet het echt Shaun!</p>';
+        break;
+        case 22:
+            modalBody.innerHTML = '<h2>Je maakt lekker eten</h2>';
+            modalBody.innerHTML += '<p>Je pasta, je wraps, je tosti, zelf je broodje ei!</p>';
+        break;
+        case 23:
+            modalBody.innerHTML = '<h2>Je maakt mij en iedereen om je heen trots</h2>';
+            modalBody.innerHTML += '<p>Ik ben trots op jou en alles wat je doet!</p>';
+        break;
+        case 24:
+            modalBody.innerHTML = '<h2>Je bent schattig</h2>';
+            modalBody.innerHTML += '<p>Vooral als je in slaap valt tijdens een film!</p>';
+        break;
+        case 25:
+            modalBody.innerHTML = '<h2>Je hebt passie in de dingen die je doet</h2>';
+            modalBody.innerHTML += '<p>Het maakt niet uit wat het is</p>';
+        break;
+        case 26:
+            modalBody.innerHTML = '<h2>Elke keer dat ik je zie lijkt het de eerste keer</h2>';
+            modalBody.innerHTML += '<p>Kben elke keer verbaasd over hoe knap je bent!</p>';
+        break;
+        case 27:
+            modalBody.innerHTML = '<h2>Je kan goed autorijden</h2>';
+            modalBody.innerHTML += '<p>Het is echt zo en kmoest het er gwn bij zetten!</p>';
+        break;
+        case 28:
+            modalBody.innerHTML = '<h2>Jij bent anders dan anderen</h2>';
+            modalBody.innerHTML += '<p>Vooral door de dingen die ik hier opnoem!</p>';
+        break;
+        case 29:
+            modalBody.innerHTML = '<h2>Je hebt leuk humor</h2>';
+            modalBody.innerHTML += '<p>Wij kunnen lachen om dezelfde dingen!</p>';
+        break;
+        case 30:
+            modalBody.innerHTML = '<h2>Wanneer je iets wilt ga je er echt voor</h2>';
+            modalBody.innerHTML += '<p>Maakt niet uit wat het is</p>';
+        break;
+        case 31:
+            modalBody.innerHTML = '<h2>Je hebt een mooie lach</h2>';
+            modalBody.innerHTML += '<p>Ik word er zelf gwn blij van!</p>';
+        break;
+        case 32:
+            modalBody.innerHTML = '<h2>Je hebt een mooi gebit</h2>';
+            modalBody.innerHTML += '<p>Valt echt elke keer op!</p>';
+        break;
+        case 33:
+            modalBody.innerHTML = '<h2>Je staat altijd schattig</h2>';
+            modalBody.innerHTML += '<p>Wanneer je met je been schuin staat</p>';
+        break;
+        case 34:
+            modalBody.innerHTML = '<h2>Je danst schattig</h2>';
+            modalBody.innerHTML += '<p>Vooral wanneer je blij bent!</p>';
+        break;
+        case 35:
+            modalBody.innerHTML = '<h2>Je ruikt lekker</h2>';
+            modalBody.innerHTML += '<p>Doet me altijd denken aan onze eerste dates!</p>';
+        break;
+        case 36:
+            modalBody.innerHTML = '<h2>Je maakt mij gelukkig</h2>';
+            modalBody.innerHTML += '<p>Met alles wat je voor me doet!</p>';
+        break;
+        case 37:
+            modalBody.innerHTML = '<h2>Je bent een model</h2>';
+            modalBody.innerHTML += '<p>Mijn model!</p>';
+        break;
+        case 38:
+            modalBody.innerHTML = '<h2>Je kan goed voetballen</h2>';
+            modalBody.innerHTML += '<p>Je freestyle tricks blijven me verbazen!</p>';
+        break;
+        case 39:
+            modalBody.innerHTML = '<h2>Je bent een goede detective</h2>';
+            modalBody.innerHTML += '<p>ff serieus hoe heb je dat hotel van Justin gevonden?</p>';
+        break;
+        case 40:
+            modalBody.innerHTML = '<h2>Je bent goed in COD</h2>';
+            modalBody.innerHTML += '<p>Ksnap nog steeds niet hoe je zo goed bent.</p>';
+        break;
+        case 41:
+            modalBody.innerHTML = '<h2>Je bent aesthetic</h2>';
+            modalBody.innerHTML += '<p>Jij en al je content!</p>';
+        break;
+        case 42:
+            modalBody.innerHTML = '<h2>Je betaalt ook voor dingen</h2>';
+            modalBody.innerHTML += '<p>Iets wat niet elke meid zou doen!</p>';
+        break;
+        case 43:
+            modalBody.innerHTML = '<h2>Je kan goed content zoeken en maken</h2>';
+            modalBody.innerHTML += '<p>Net zoals de content op je tiktoks van HealthyMindset en je eigen tiktok!</p>';
+        break;
+        case 44:
+            modalBody.innerHTML = '<h2>Je bent een harde werker</h2>';
+            modalBody.innerHTML += '<p>Je werkt echt enorm hard en kben enorm trots op je!</p>';
+        break;
+        case 45:
+            modalBody.innerHTML = '<h2>Je hebt een leuke muzieksmaak</h2>';
+            modalBody.innerHTML += '<p>Net zoals we bij de eerste jam achter kwamen!</p>';
+        break;
+    }
+
+    document.getElementById('heartModal').style.display = 'flex'; // Use flex for centering
+}
+
+function closeModal() {
+    document.getElementById('heartModal').style.display = 'none';
+}
+
+// Close the modal when clicking outside of the modal content
+window.onclick = function(event) {
+    const modal = document.getElementById('heartModal');
+    if (event.target === modal) {
+        closeModal();
+    }
+};
+
+function ShowNavBar(){
+    const testElement = document.querySelector('.navbar'); // Select the element with class 'test'
+            
+    // Check current display style and toggle
+    if (testElement.style.display === 'none' || testElement.style.display === '') {
+        testElement.style.display = 'flex'; // Change to display flex
+    } else {
+        testElement.style.display = 'none'; // Change back to none
+    }
+
+}
